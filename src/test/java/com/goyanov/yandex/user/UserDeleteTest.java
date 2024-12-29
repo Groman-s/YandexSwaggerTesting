1aa10564aa33137b0a1b8f9c53c25e55bcf496f8
Groman-S <goyanov99@mail.ru>
2024-12-29 13:29:43 +0300

    Закончил тестирование пользовательского API.

---
+++ src/test/java/com/goyanov/yandex/user/UserDeleteTest.java
@@ -0,0 +1,36 @@
+package com.goyanov.yandex.user;
+
+import com.goyanov.yandex.swagger.openapi.testing.api.UserApi;
+import com.goyanov.yandex.swagger.openapi.testing.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.HttpClientErrorException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserDeleteTest
+{
+    @Autowired
+    private UserApi userApi;
+
+    @Test
+    public void deleteUser_Successful()
+    {
+        userApi.createUser(new User().id(34L).username("leha"));
+        userApi.deleteUser("leha");
+    }
+
+    @Test
+    public void deleteUser_InvalidUsername()
+    {
+        assertThrows(HttpClientErrorException.class, () -> userApi.deleteUser(null));
+    }
+
+    @Test
+    public void deleteUser_NotFound()
+    {
+        assertThrows(HttpClientErrorException.NotFound.class, () -> userApi.deleteUser("7ayDW&A &AWDjhi (AP(iuh a"));
+    }
+}
